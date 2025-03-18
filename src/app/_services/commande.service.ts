9f3df3a12c3f54f9414e5be4d1339ed1a2aac35d
p.bossis <pbossis@hotmail.com>
2025-03-18 14:29:16 +0100

    idk

--- src/app/_services/commande.service.ts
+++ src/app/_services/commande.service.ts
@@ -9,26 +9,26 @@ import { environment } from './_environments/environment';
   providedIn: 'root'
 })
 export class CommandeService {
-  authService:AuthService = inject(AuthService)
-  httpClient:HttpClient = inject(HttpClient)
-  apiUrl:string = environment.apiUrl
-  basket:Commande_Produit[] = []
+  authService: AuthService = inject(AuthService)
+  httpClient: HttpClient = inject(HttpClient)
+  apiUrl: string = environment.apiUrl
+  basket: Commande_Produit[] = []
   private readonly basketCountSubject: BehaviorSubject<number> = new BehaviorSubject<number>(0)
 
-  constructor(){
+  constructor() {
     this.initBasket()
   }
 
-  getAllCommandesAdmin():Observable<Commandes[]>{
+  getAllCommandesAdmin(): Observable<Commandes[]> {
     return this.httpClient.get<Commandes[]>(this.apiUrl + 'commande')
   }
 
-  getCommandeById(id:number):Observable<Commandes>{
+  getCommandeById(id: number): Observable<Commandes> {
     return this.httpClient.get<Commandes>(this.apiUrl + 'commande/' + id)
   }
 
-  createCommande(basket:Commande_Produit[]):Observable<string>{
-    return this.httpClient.post<string>(this.apiUrl + 'commande/create-commande', basket,{responseType: 'text' as 'json'}).pipe(
+  createCommande(basket: Commande_Produit[]): Observable<string> {
+    return this.httpClient.post<string>(this.apiUrl + 'commande/create-commande', basket, { responseType: 'text' as 'json' }).pipe(
       tap(() => {
         this.basket = []
         this.basketCountSubject.next(0)
@@ -37,13 +37,13 @@ export class CommandeService {
     )
   }
 
-  updateEtatCommande(id:number,stateId:number):Observable<any>{
-    return this.httpClient.put<any>(this.apiUrl + 'commande/update-state/' + id + '?stateId=' +  stateId, {})
+  updateEtatCommande(id: number, stateId: number): Observable<any> {
+    return this.httpClient.put<any>(this.apiUrl + 'commande/update-state/' + id + '?stateId=' + stateId, {})
   }
 
-    get basketCount$(): Observable<number> {
-      return this.basketCountSubject.asObservable();
-    }
+  get basketCount$(): Observable<number> {
+    return this.basketCountSubject.asObservable();
+  }
 
 
 
@@ -51,29 +51,29 @@ export class CommandeService {
 
 
 
-    initBasket(){
-      const storedBasket = localStorage.getItem('basket' + this.authService.getId());
-      this.basket = storedBasket ? JSON.parse(storedBasket) : []
-      this.basketCountSubject.next(this.basket.length)
-    }
-  AddToBasket(cp:Commande_Produit){
-    if(cp.quantite < 1 || cp.produitId < 1) return false
+  initBasket() {
+    const storedBasket = localStorage.getItem('basket' + this.authService.getId());
+    this.basket = storedBasket ? JSON.parse(storedBasket) : []
+    this.basketCountSubject.next(this.basket.length)
+  }
+  AddToBasket(cp: Commande_Produit) {
+    if (cp.quantite < 1 || cp.produitId < 1) return false
     const existingProductIndex = this.basket.findIndex(item => item.produitId == cp.produitId);
-  
+
     if (existingProductIndex !== -1) {
       this.basket[existingProductIndex].quantite += cp.quantite
     }
     else {
       this.basket.push(cp);
-      this.basketCountSubject.next(this.basketCountSubject.value + 1)     
+      this.basketCountSubject.next(this.basketCountSubject.value + 1)
     }
     localStorage.setItem('basket' + this.authService.getId(), JSON.stringify(this.basket))
     return true
   }
 
-  RemoveFromBasket(index:number){
-    this.basket.splice(index,1)
-    this.basketCountSubject.next(this.basketCountSubject.value -1)
+  RemoveFromBasket(index: number) {
+    this.basket.splice(index, 1)
+    this.basketCountSubject.next(this.basketCountSubject.value - 1)
     localStorage.setItem('basket' + this.authService.getId(), JSON.stringify(this.basket))
   }
 
